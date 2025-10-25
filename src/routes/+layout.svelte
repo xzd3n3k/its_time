8788607a7fd2ca02440d935b60635e9eafefff44
Zdeněk Němec <xzdeneknemec@outlook.cz>
2025-10-25 15:41:54 +0200

    feat(assets): preload icons and images

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,10 +5,28 @@
     import { Toaster } from 'svelte-sonner';
 
 	let { children } = $props();
+
+    const iconsAndImages = [
+        '/red_audi_back.jpeg',
+        '/phone.svg',
+        '/email.svg',
+        '/location.svg',
+        '/hand_wash.svg',
+        '/standard_package.svg',
+        '/premium_package.svg',
+        '/wax.svg',
+        '/paint_correction.svg',
+        '/ceramic_coating.svg',
+        '/ozone_disinfection.svg',
+        '/logo_zakladni.svg',
+    ];
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+    {#each iconsAndImages as icon, i (i)}
+        <link rel="preload" as="image" href="{icon}">
+    {/each}
 </svelte:head>
 
 <Navbar></Navbar>
