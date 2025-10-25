15fb8a4598c2b2d97db4b95cd2d2703b04aac572
Zdeněk Němec <xzdeneknemec@outlook.cz>
2025-10-25 19:00:48 +0200

    feat(dialog): add header content and footer sections, add close button icon

--- src/Dialog.svelte
+++ src/Dialog.svelte
@@ -1,8 +1,45 @@
-<script>
-    export let dialog
+<script lang="ts">
+    export let dialog: HTMLDialogElement;
+    export let title = '';
+    export let showClose = true;
+
+    function close() {
+        dialog.close();
+    }
 </script>
+
 <dialog bind:this={dialog} on:close>
-    <slot/>
+    <div class="dialog-container">
+        {#if title || showClose}
+            <header class="dialog-header">
+                {#if title}
+                    <h2>{title}</h2>
+                {/if}
+                {#if showClose}
+                    <button class="close-button" on:click={close} aria-label="Zavřít">
+                        <img
+                            src="/cross.svg"
+                            width="24"
+                            height="24"
+                            alt="zavrit"
+                        />
+                    </button>
+                {/if}
+            </header>
+        {/if}
+
+        <section class="dialog-content">
+            <slot name="content">
+                <slot />
+            </slot>
+        </section>
+
+        {#if $$slots.footer}
+            <footer class="dialog-footer">
+                <slot name="footer" />
+            </footer>
+        {/if}
+    </div>
 </dialog>
 
 <style>
@@ -11,7 +48,6 @@
         border: none;
         z-index: 300;
 
-        padding: 1rem 2rem;
         border-radius: 10px;
         background: linear-gradient(180deg, hsl(220 18% 12%) 0%, hsl(220 20% 10%) 100%);
 
@@ -25,5 +61,46 @@
 
     dialog::backdrop {
         background: rgba(0, 0, 0, 0.5);
+    }
+
+    .dialog-container {
+        display: flex;
+        flex-direction: column;
+        min-width: 300px;
+        max-width: 90vw;
+        max-height: 90vh;
+    }
+
+    .dialog-header {
+        display: flex;
+        flex-direction: row-reverse;
+        justify-content: space-between;
+        align-items: center;
+        padding: 1rem;
+        border-bottom: 1px solid #ddd;
+    }
+
+    .dialog-header h2 {
+        margin: 0;
+        font-size: 1.1rem;
+    }
+
+    .close-button {
+        display: inline-flex;
+        background: none;
+        border: none;
+    }
+
+    .dialog-content {
+        padding: 1rem 2rem;
+        overflow-y: auto;
+    }
+
+    .dialog-footer {
+        padding: 0.75rem 1rem;
+        border-top: 1px solid #ddd;
+        display: flex;
+        justify-content: flex-end;
+        gap: 0.5rem;
     }
 </style>
