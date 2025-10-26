7793fcda73080f8185a8e8b564adf9a27f49d6ef
Zdeněk Němec <xzdeneknemec@outlook.cz>
2025-10-26 11:42:34 +0100

    feat(StatsCard): add statscard component

---
+++ src/routes/StatsCard.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+    import { tweened } from 'svelte/motion';
+    import { cubicOut } from 'svelte/easing';
+    import { onMount } from 'svelte';
+
+    const stats = [
+        { label: 'Realizovaných zakázek', value: 360 },
+        { label: 'Hodin ročně strávených čištěním vozidel', value: 1200 },
+        { label: 'Stálých klientů', value: 80 },
+        { label: 'Let zkušeností', value: 10 },
+    ];
+
+    let counter1 = tweened(0, { duration: 2000, easing: cubicOut });
+    let counter2 = tweened(0, { duration: 2000, easing: cubicOut });
+    let counter3 = tweened(0, { duration: 2000, easing: cubicOut });
+    let counter4 = tweened(0, { duration: 2000, easing: cubicOut });
+
+    onMount(() => {
+        counter1.set(stats[0].value);
+        counter2.set(stats[1].value);
+        counter3.set(stats[2].value);
+        counter4.set(stats[3].value);
+    });
+</script>
+
+<div class="stats-card">
+    <div class="stat">
+        <div class="number">{Math.round($counter1)}+</div>
+        <div class="label">{stats[0].label}</div>
+    </div>
+
+    <div class="stat">
+        <div class="number">{Math.round($counter2)}+</div>
+        <div class="label">{stats[1].label}</div>
+    </div>
+
+    <div class="stat">
+        <div class="number">{Math.round($counter3)}</div>
+        <div class="label">{stats[2].label}</div>
+    </div>
+
+    <div class="stat">
+        <div class="number">{Math.round($counter4)}+</div>
+        <div class="label">{stats[3].label}</div>
+    </div>
+</div>
+
+<style>
+    .stats-card {
+        display: flex;
+        justify-content: space-around;
+        padding: 12px;
+        color: var(--color-accent);
+        border-radius: 12px;
+        box-shadow: 0 5px 15px rgba(0,0,0,0.1);
+        text-align: center;
+        font-family: sans-serif;
+    }
+
+    .stat {
+        flex: 1;
+        margin: 0 1rem;
+        align-content: center;
+    }
+
+    .number {
+        font-size: 2rem;
+        font-weight: bold;
+    }
+
+    .label {
+        margin-top: 0.5rem;
+        color: var(--color-text-secondary);
+    }
+</style>
